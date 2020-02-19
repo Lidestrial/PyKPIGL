4ba937077f4a7d7485f619c876d5d3eac9b7e2c6
Serzh <ushakovsergey21@gmail.com>
2020-02-19 17:22:23 +0200

    newnext

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from flask import Flask, escape, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('first')
 def hello():
     name = request.args.get("name", "World")
+    num=24
+    st='Chiki-briki'
+    b=None
+    return f(num, st, b)
     return f'Sergey Ushakov, TI-71'
     
 if __name__ == '__main__':
