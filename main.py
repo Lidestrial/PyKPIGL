fd7e47758b6b915d0fc2f45087cefe01acd1c391
Serzh <ushakovsergey21@gmail.com>
2020-02-19 17:36:27 +0200

    initial

--- main.py
+++ main.py
@@ -5,11 +5,17 @@ app = Flask(__name__)
 @app.route('/first')
 def hello():
     name = request.args.get("name", "World")
+    b=None
+    return f'Sergey Ushakov, TI-71'
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
     num=24
     st='Chiki-briki'
     b=None
     return f'{num}, {st}, {b}
     return f'Sergey Ushakov, TI-71'
-    
+
 if __name__ == '__main__':
     app.run('0.0.0.0')
