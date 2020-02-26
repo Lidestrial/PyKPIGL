e7597340e566720ec32b3683d3ab31ea5f9e6811
Serzh <ushakovsergey21@gmail.com>
2020-02-26 18:40:13 +0200

    D:/Git/second pre rel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # @SEENPAY
-from flask import Flask, escape, request
-
+from flask import Flask, escape, request,render_template
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,6 +13,27 @@ def helloo():
     num = 42
     damki = 'chiki-briki'
     return f'{chpek}, {num}, {damki}'
+
+@app.route('/second', methods=['GET','POST'])
+def second():
+    if  request.method == 'GET':
+        return render_template('index.html')
+    if request.method == 'POST':
+        test = request.form.get('text')
+        print(test)
+     
+        rev = [a for a in reversed(test)] 
+        lst=request.form.get('text')
+        return f'{lst[::-1]} {list(reversed(lst))}, {rev}\n'
+
+
+    
+    # lst=request.form.get("text")
+    # return f'{reverse(lst)}\n'
+
+# # def reverse3():
+# #     lst=request.form.get("text")
+# #     return f'{[ for i in lst.reversed]}\n'
 
 if __name__ == '__main__':
     app.run('0.0.0.0')
