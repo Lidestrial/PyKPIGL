7a0dc8cf0240886d4ea5be0bd6bf11a0d02e8146
Serzh <ushakovsergey21@gmail.com>
2020-03-03 20:27:24 +0200

    Third commit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # @SEENPAY
 from flask import Flask, escape, request,render_template
+from deck import Deck
+
 app = Flask(__name__)
+deck = Deck()
 
 @app.route('/')
 def hello():
@@ -17,23 +20,33 @@ def helloo():
 @app.route('/second', methods=['GET','POST'])
 def second():
     if  request.method == 'GET':
-        return render_template('index.html')
+        return render_template('second.html')
     if request.method == 'POST':
-        test = request.form.get('text')
-        print(test)
-     
-        rev = [a for a in reversed(test)] 
+
         lst=request.form.get('text')
-        return f'{lst[::-1]} {list(reversed(lst))}, {rev}\n'
+        comprehension  = "".join([a for a in reversed(lst)]) 
+        rev ="".join(list(reversed(lst)))
+        sl ="".join(lst[::-1])
+        # 
+        return f'{comprehension}<br> {rev}<br> {sl}\n'
 
+@app.route('/third', methods=['GET','POST'])
+def third():
+    if  request.method == 'GET':
+        return render_template('./third.html',content='\u0020',  deck=str(deck))
+    if request.method == 'POST':
+        if "shuffle" in request.form:
+            deck.shuffle()
+            message = "Deck was shuffled"
+        elif "pop" in request.form:
+            message = deck.pop()
+        elif "get_random" in request.form:
+            message = deck.get_random()
+        else:
+            num = request.form.get("index")
+            message = deck.index(num)
+        return render_template('third.html', content=message, deck=str(deck))
 
-    
-    # lst=request.form.get("text")
-    # return f'{reverse(lst)}\n'
-
-# # def reverse3():
-# #     lst=request.form.get("text")
-# #     return f'{[ for i in lst.reversed]}\n'
 
 if __name__ == '__main__':
     app.run('0.0.0.0')
